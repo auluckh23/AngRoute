050dd8396d4797679aeef5fac99dae2585518180
Harsharn <hauluck@gmail.com>
2015-06-18 12:08:53 -0700

    Inital Git Commit

---
+++ WebContent/js/AngRoute.js
@@ -0,0 +1,16 @@
+var app = angular.module("AngRoute",['ngRoute']);
+
+app.controller("homecont", function($scope){
+	$scope.subs=function(){
+		
+	};
+	
+});
+
+app.controller("biocont", function(){
+	
+});
+
+app.controller("contactcont", function(){
+	
+});
