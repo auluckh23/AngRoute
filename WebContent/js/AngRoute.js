edec74c586fd57965cfb6aa77516fa9c474495fa
Harsharn <hauluck@gmail.com>
2015-06-18 12:46:52 -0700

    Working Home and AngRoute.js

--- WebContent/js/AngRoute.js
+++ WebContent/js/AngRoute.js
@@ -1,8 +1,17 @@
 var app = angular.module("AngRoute",['ngRoute']);
 
 app.controller("homecont", function($scope){
+	$scope.fdi=true;
+	$scope.sdi=false;
+	
 	$scope.subs=function(){
-		
+		$scope.sdi=true;
+		$scope.fdi=false;
+		var dats = new Date();
+		var hrs = dats.getHours();
+		if(hrs < 12)
+		{$scope.datey ="Morning"}
+		else {$scope.datey ="Evening"}
 	};
 	
 });
@@ -13,4 +22,17 @@ app.controller("biocont", function(){
 
 app.controller("contactcont", function(){
 	
+});
+
+app.config(function($routeProvider){
+	$routeProvider.when("/home",{
+		templateUrl:"home.html",
+		controller:"homecont"
+	}).when("/bio",{
+		templateUrl:"bio.html",
+		controller: "biocont"
+	}).when("/contact",{
+		templateUrl:"contact.html",
+		controller: "contactcont"
+	}).otherwise({redirectTo: '/home'});
 });
